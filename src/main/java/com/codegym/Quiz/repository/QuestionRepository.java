5952917067e1c65948fc1c6602546a31957441ab
TranNgo <ntbtcv@gmail.com>
2026-08-18 20:59:50 +0700

    sửa lỗi findByCategoryId

--- src/main/java/com/codegym/Quiz/repository/QuestionRepository.java
+++ src/main/java/com/codegym/Quiz/repository/QuestionRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findByIdNotIn(List<Long> ids);
+    List<Question> findByCategoryId(Long categoryId);
 }
